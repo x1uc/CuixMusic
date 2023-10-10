047a8607ac203e6a5ccfbbcb0b26ae475bce5c93
cuix <704379838@qq.com>
2023-10-10 21:00:42 +0800

    服务器布置代码，修改了一下sql 。

--- CuixMusic.sql
+++ CuixMusic.sql
@@ -1,14 +1,17 @@
+CREATE DATABASE ssm_db;
+
+
 CREATE TABLE CuixMusic
 (
     id       INT PRIMARY KEY AUTO_INCREMENT,
     name     VARCHAR(255) NOT NULL,
-    path     VARCHAR(255) NOT NULL,c
+    path     VARCHAR(255) NOT NULL,
     musician VARCHAR(255) NOT NULL,
     arr_img  VARCHAR(255) NOT NULL
 );
 
 insert into CuixMusic
-values (1, "星晴", "./song/星晴.mp3", "周杰伦", "/images/jay.png"),
-       (2, "暗号", "./song/暗号.mp3", "周杰伦", "/images/八度空间.png"),
-       (3, "晴天", "./song/晴天.mp3", "周杰伦", "/images/叶惠美.png"),
-       (4, "七里香", "./song/七里香.mp3", "周杰伦", "/images/七里香.png")
+values (1, "星晴", "/song/星晴.mp3", "周杰伦", "/images/jay.png"),
+       (2, "暗号", "/song/暗号.mp3", "周杰伦", "/images/八度空间.png"),
+       (3, "晴天", "/song/晴天.mp3", "周杰伦", "/images/叶惠美.png"),
+       (4, "七里香", "/song/七里香.mp3", "周杰伦", "/images/七里香.png")
